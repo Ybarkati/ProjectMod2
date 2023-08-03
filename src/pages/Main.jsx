4384894900b9fbc4d4a78af3c52d99adfca8f9de
Ybarkati <yassinebarkati47@gmail.com>
2023-08-03 14:46:05 -0600

    done9

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom"
 import { useTool } from "../components/ContextTools"
 import "../components/init"
+import {background1} from "../assets/background1.png"
 
 export default function Main(){
     const navigate=useNavigate()
@@ -8,7 +9,7 @@ export default function Main(){
     return(
         <div className="Main" style={{marginLeft:Open? "160px":"80px",transition:" margin 300ms"}}>
             
-            <img src="./src/assets/background1.png" alt="" />
+            <img src={background1} alt="" />
              <button onClick={()=>navigate("/books")}>Get Your Book</button>
         </div>
     )
